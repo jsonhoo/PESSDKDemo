ed75f7425f01af62c8f75c4a57d43e77c1526b48
lc <513036862@qq.com>
2021-01-19 15:52:32 +0800

    主页面布局重构

--- app/src/main/java/ai/pensees/sdkdemo/HomeActivity.java
+++ app/src/main/java/ai/pensees/sdkdemo/HomeActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
